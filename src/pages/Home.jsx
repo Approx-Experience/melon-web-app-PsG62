6aaaa4986242f6b947a8a1be3ca3fc38e4f75f6b
rileyeh <rhatchdev@gmail.com>
2025-02-10 15:05:35 -0700

    complete homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,66 +1,64 @@
-import { Container, Row, Col, Button } from "react-bootstrap";
-import "./Home.css";
+import './Home.css'
+
+import { Button, Col, Container, Row } from 'react-bootstrap'
 
 const Home = () => {
-
   return (
     <Container fluid>
-    <Row className="banner-1">
-        <Col className="banner-1 title">
-          <h5 className="banner-1">SUMMER 2024</h5>
-          <h1 className="banner-1">NEW COLLECTION</h1>
-          <h4 className="banner-1">shorts, tees, tanks & more!</h4>
-          <Button className="banner-1">
-            <h3 className="banner-1">SHOP NOW</h3>
+      <Row className='banner-1'>
+        <Col className='banner-1 title'>
+          <h5 className='banner-1'>SUMMER 2024</h5>
+          <h1 className='banner-1'>NEW COLLECTION</h1>
+          <h4 className='banner-1'>shorts, tees, tanks & more!</h4>
+          <Button className='banner-1'>
+            <h3 className='banner-1'>SHOP NOW</h3>
           </Button>
         </Col>
-        <Col className="banner-1-img-col">
+        <Col className='banner-1-img-col'>
           <img
-            src="/banner-1.png"
-            alt=""
-            className="banner-1 image"
-            width="415px"
+            src='/banner-1.png'
+            alt=''
+            className='banner-1 image'
+            width='415px'
           />
         </Col>
       </Row>
-      <Row className="categories-title">
-        <h3 className="categories-title">shop by category</h3>
-        <h6 className="categories-title">whatcha lookin' for?</h6>
+      <Row className='categories-title'>
+        <h3 className='categories-title'>shop by category</h3>
+        <h6 className='categories-title'>whatcha lookin' for?</h6>
       </Row>
-      <Row className="categories-row">
-        <Col className="categories shirts-col">
-          <a href="#shirts">
-            <img src="/shirts.png" alt="" className="shirts" />
+      <Row className='categories-row'>
+        <Col className='categories shirts-col'>
+          <a href='#shirts'>
+            <img src='/shirts.png' alt='' className='shirts' />
           </a>
         </Col>
-        <Col className="categories shorts-col">
-          <a href="#shorts">
-            <img src="/shorts.png" alt="" className="shorts" />
+        <Col className='categories shorts-col'>
+          <a href='#shorts'>
+            <img src='/shorts.png' alt='' className='shorts' />
           </a>
         </Col>
-        <Col className="categories hats-col">
-          <a href="#hats" className="hats-link">
-            <img src="/hats.png" alt="" height="232px" className="hats" />
+        <Col className='categories hats-col'>
+          <a href='#hats' className='hats-link'>
+            <img src='/hats.png' alt='' height='232px' className='hats' />
           </a>
-          <a href="#athleisure" className="athleisure-link">
+          <a href='#athleisure' className='athleisure-link'>
             <img
-              src="/athleisure.png"
-              alt=""
-              height="232px"
-              className="athleisure"
+              src='/athleisure.png'
+              alt=''
+              height='230px'
+              className='athleisure'
             />
           </a>
         </Col>
       </Row>
-      <Row className="best-sellers">
-        <h3 className="best-sellers">best sellers</h3>
-        <h6 className="best-sellers">get 'em while you can</h6>
+      <Row className='best-sellers'>
+        <h3 className='best-sellers'>best sellers</h3>
+        <h6 className='best-sellers'>get 'em while you can</h6>
       </Row>
-      <Row className="home-products-row">
-        //future products
-      </Row>
+      <Row className='home-products-row'>//future products</Row>
     </Container>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
